34b39a1676e753b67d1f42654e4ce67c2b65a19d
malekalabed <57950278+malekalabed@users.noreply.github.com>
2019-12-03 14:53:00 +0100

    added the sub method class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,13 +1,19 @@
 package com.company;
 
 public class Math {
-    public int  add(int first, int second) {
+    public int add(int first, int second) {
 
-       int sum =  first + second;
-       System.out.println(sum);
-       return sum;
+        int sum = first + second;
+        System.out.println(sum);
+        return sum;
     }
 
+    public int sub(int first, int second) {
 
+        int sum = first - second;
+        System.out.println(sum);
+        return sum;
+
+
+    }
 }
-
