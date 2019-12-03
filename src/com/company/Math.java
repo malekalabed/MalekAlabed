99b5bdbfdae0144b49ca5b431f044618dd6e76ce
malekalabed <57950278+malekalabed@users.noreply.github.com>
2019-12-03 14:32:26 +0100

    new changes

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class Math {
+    public static void main(String []args) {
+        int first = 5, second = 5;
+        int sum = first + second;
+        System.out.println(sum);
+    }
+
+
 }
